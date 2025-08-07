cbc0be2d704f532a69f0e555a2f83adf4ab491f7
Abhishek Gautam <160625532+iabhishekgautam@users.noreply.github.com>
2025-08-07 11:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function showMessage() {
     alert('Login functionality coming soon!');
 }
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    window.location.href = "dashboard.html";
+  })
+  .catch((error) => {
+    alert(error.message);
+  });
